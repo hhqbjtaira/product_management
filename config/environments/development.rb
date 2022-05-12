526757baeab719ddfa5e5491997bcfc19b13e33d
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-12 13:51:37 +0900

    bullet 設定の全角スペースの修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = true #Bullet gemを有効
-    Bullet.alert = true　#ブラウザにJavaScriptアラートをポップアップ
+    Bullet.alert = true #ブラウザにJavaScriptアラートをポップアップ
     Bullet.bullet_logger = true #Bulletログファイル（Rails.root/log/bullet.log）に記録
     Bullet.console = true #警告をブラウザーのconsole.logに記録
     Bullet.rails_logger = true #警告を直接Railsログに追加
