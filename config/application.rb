2ba4adc9e965a323a88b22b59da82c4e998930f3
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-13 16:43:02 +0900

    不要なファイルを作成しないための設定を追記

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,19 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.template_engine false
+      g.test_framework :rspec,
+                        fixtures: true,
+                        view_spec: false,
+                        routing_specs: false,
+                        helper_specs: false,
+                        controller_specs: false,
+                        request_specs: true
+    end
   end
 end
