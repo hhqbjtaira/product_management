ef16675602840735e38ce72b4e30eb6657d3d7c1
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-16 17:10:02 +0900

    product関連 Migrationファイルの追記

--- db/migrate/20220516075252_create_products.rb
+++ db/migrate/20220516075252_create_products.rb
@@ -2,8 +2,8 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.references :category_id, null: false, foreign_key: true
-      t.string :name
-      t.text :description
+      t.string :name, null: false
+      t.text :description, null: false
       t.datetime :create_at
       t.datetime :updated_at
 
