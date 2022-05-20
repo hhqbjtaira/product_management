101abed9ec021636eae6e2a8c743615d4f353481
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-20 15:48:00 +0900

    category Rspecに一意の制約を追記

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -13,23 +13,32 @@ RSpec.describe Category, type: :model do
   describe 'バリデーションのテスト' do
     # factoriesで作成したダミーデータを使用します。
     let(:category) { FactoryBot.create(:category) }
-    let!(:product) { build(:product, category_id: category.id) }
+    # let!(:product) { build(:product, category_id: category.id) }
 
-    # test_postを作成し、空欄での登録ができるか確認します。
-    subject { test_product.valid? }
-    let(:test_product) { product }
+    # test_categoryを作成し、空欄での登録ができるか確認します。
+    subject { test_category.valid? }
+    let(:test_category) { category }
     
     context 'nameカラム' do
       it '空欄でないこと' do
-        test_post.title = ''
+        test_category.name = ''
         is_expected.to eq false;
       end
-
-      # it do
-      #   expect(subject).to be_valid
-      # end
     end
 
+    context '一意の制約を確かめる' do
+      before do
+        @category = FactoryBot.build(:category)
+      end
+
+      it '同じカテゴリーは登録できないこと' do
+        @category.save 
+        another_category = FactoryBot.build(:category) 
+        another_category.name = @category.name
+        another_category.valid?
+        expect(another_category.errors.full_messages).to include("このカテゴリーはすでに存在します")
+      end
+    end
 
   end  
 end
