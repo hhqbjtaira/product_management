9e9ad41a3847d21a7b97d7424dcfe094709cd449
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-20 15:03:10 +0900

    category Rspec作成

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,5 +10,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    # factoriesで作成したダミーデータを使用します。
+    let(:category) { FactoryBot.create(:category) }
+    let!(:product) { build(:product, category_id: category.id) }
+
+    # test_postを作成し、空欄での登録ができるか確認します。
+    subject { test_product.valid? }
+    let(:test_product) { product }
+    
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        test_post.title = ''
+        is_expected.to eq false;
+      end
+
+      # it do
+      #   expect(subject).to be_valid
+      # end
+    end
+
+
+  end  
 end
