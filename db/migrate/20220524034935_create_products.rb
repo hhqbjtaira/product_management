7f0a034e6df7789e82d0f431427722e4af1c82e0
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-24 16:29:16 +0900

    #6 マイグレーションファイルを追加

--- db/migrate/20220524034935_create_products.rb
+++ db/migrate/20220524034935_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.references :category_id, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.string :name, null: false
       t.text :description, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_081554) do
+ActiveRecord::Schema.define(version: 2022_05_24_034935) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -18,4 +18,14 @@ ActiveRecord::Schema.define(version: 2022_05_23_081554) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
