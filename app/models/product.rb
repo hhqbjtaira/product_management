997262e869fb1a8bbff990758de0bbc59f71612a
hhqbjtaira <hhqbjjob@gmail.com>
2022-05-24 18:38:36 +0900

    #6 categoryとproductのリレーションを記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,26 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)      not null
+#  name        :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
 class Product < ApplicationRecord
   belongs_to :category, foreign_key: :category_id
   with_options presence: true do
-    validates :category_id, foreign_key: true
+    validates :category_id
     validates :name
     validates :description
   end
